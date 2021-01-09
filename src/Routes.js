6bb18b2e0bad54b154d0c5713c66511ef82d23fb
Xavier Defontaine <xdefontaine@gmail.com>
2021-01-09 15:03:41 +0000

    Added 404 page

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Home from "./containers/Home";
+import NotFound from  "./containers/NotFound";
 import { Switch, Route } from "react-router-dom";
+
 
 export default function Routes() {
   return(
     <Switch>
       <Route exact path="/">
         <Home />
+      </Route>
+      <Route>
+        <NotFound />
       </Route>
     </Switch>
   )};
---
+++ src/containers/NotFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./NotFound.css";
+
+export default function NotFound() {
+  return(
+    <div className="notFound text-center">
+      <h3>Sorry, page not found!</h3>
+      <img src="404.png" atl="404.png" ></img>
+    </div>
+  );
+}
