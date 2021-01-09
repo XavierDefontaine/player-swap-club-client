5b664c05e2edc2a9605b2a203f8119fb322b876d
Xavier Defontaine <xdefontaine@gmail.com>
2021-01-09 13:06:24 +0000

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import Nav from "react-bootstrap/Nav";
+import React from "react";
+import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
+import Navbar from "react-bootstrap/Navbar";
+import "./App.css";
+import Routes from "./Routes"; 
+import { LinkContainer } from "react-router-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container py-3">
+      <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
+      <LinkContainer to="/">
+        <Navbar.Brand className="font-weight-bold text-muted">
+        PlayerSwap Club 
+        </Navbar.Brand>
+      </LinkContainer>
+        <Navbar.Toggle />
+          <NavbarCollapse className="justify-content-end">
+            <LinkContainer to="/signup">
+            <Nav.Link >Sign Up</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/login">
+            <Nav.Link>Login</Nav.Link>
+            </LinkContainer>
+          </NavbarCollapse>
+      </Navbar>
+      <Routes />
     </div>
   );
 }
